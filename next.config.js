a6fcec81ec6ed3c37781f00a0f735026bb2ce897
dharmendraktek <dharmendra.patel@ktekresourcing.com>
2024-09-12 05:32:59 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,17 @@ const nextConfig = {
         NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,          // API URL
         CLIENT_URL: process.env.CLIENT_URL,    // Client URL
       },
+    path: {
+    basePath: "https://katalixai.com",
+    assetPrefix: "https://katalixai.com",
+  },
+     basePath: '/superio',  // Assuming your app is located under `/superio`
+
+    // Serve static assets (CSS, JS) from a custom path
+    assetPrefix: '/superio/_next/',
+
+    // Optional: Enforce trailing slashes for URLs and assets
+    trailingSlash: true,
 }
 
 module.exports = nextConfig
