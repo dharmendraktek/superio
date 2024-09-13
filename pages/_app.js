ef8c01eb30322e0e4aa62400e750385b8f5caa05
Dharmendra Patel <dharmendra.patel@ktekresourcing.com>
2024-09-13 09:25:42 -0400

    update app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 // pages/_app.js
 import '../styles/index.scss';  // Import global styles
-import '../styles/test.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
